6fa8a9ec371d6bc476c317ce95eb5427be20e99b
riematsubara <rie_matsubara@hotmail.com>
2021-06-10 16:02:34 -0700

    Uploaded files.

---
+++ TestPackage.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace RieMatsubara
+{
+    public class TestPackage : MonoBehaviour
+    {
+        void Start()
+        {
+            Debug.Log("Hello from Custom Package script");
+        }
+    }
+}
